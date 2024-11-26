67a93da4a6a47474d268835cf4afa163575f94e9
isaac <isaac.gonade@educa.jcyl.es>
2024-11-26 09:06:50 +0100

    Commit Menu Opciones

---
+++ src/main/java/org/example/practica_final_hibernate/Controllers/MenuController.java
@@ -0,0 +1,42 @@
+package org.example.practica_final_hibernate.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button CrearParteBtt;
+
+    @FXML
+    private Button crearProfesorBtt;
+
+    @FXML
+    private Button listaAlumnosBtt;
+
+    @FXML
+    private Button listaPartesBtt;
+
+    @FXML
+    void onCrearParte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCrearProfesor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onListaAlumnos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onListaPartes(ActionEvent event) {
+
+    }
+
+}
+
