9f94469f95b5e6767768951224bd174288f09562
isaac <isaac.gonade@educa.jcyl.es>
2024-11-26 09:44:12 +0100

    Commit Crear Parte

--- src/main/java/org/example/practica_final_hibernate/Controllers/CrearParteController.java
+++ src/main/java/org/example/practica_final_hibernate/Controllers/CrearParteController.java
@@ -1,4 +1,62 @@
 package org.example.practica_final_hibernate.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.example.practica_final_hibernate.Model.Profesor;
+
 public class CrearParteController {
+
+    @FXML
+    private Button NaranjaBtt;
+
+    @FXML
+    private Button RojoBtt;
+
+    @FXML
+    private Button VerdeBtt;
+
+    @FXML
+    private Button crearBtt;
+
+    @FXML
+    private TextArea descripcionTArea;
+
+    @FXML
+    private TextField expAlumnoTF;
+
+    @FXML
+    private DatePicker fechaPicker;
+
+    @FXML
+    private ComboBox<String> nombreGrupoCB;
+
+    @FXML
+    private ComboBox<Profesor> profesorCB;
+
+    @FXML
+    void onCrear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onParteNaranja(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onParteRojo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onParteVerde(ActionEvent event) {
+
+    }
+
 }
+
